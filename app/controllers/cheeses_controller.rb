832d2480c795de285dd068967fcb149e82d2b853
afhare <alexandra.f.hare@gmail.com>
2019-10-18 17:02:00 -0500

    Adjusted errors in view rendering for proper data display.

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,2 +1,9 @@
 class CheesesController < ApplicationController
+    def index
+        @cheeses = Cheese.all
+    end
+    
+    def show
+        @cheese = Cheese.find_by(id: params[:id])
+    end
 end
