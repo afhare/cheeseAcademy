418f494acef1d7781b4dbbf0217e647258dec27f
afhare <alexandra.f.hare@gmail.com>
2019-10-20 18:14:54 -0500

    Added cheese flavor profile to individual show page. Considering alternative methods for rendering profile to accommodate a chart or graph of the flavor wheel.

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,5 +5,17 @@ class CheesesController < ApplicationController
     
     def show
         @cheese = Cheese.find_by(id: params[:id])
+        cheese_flavors = @cheese.flavors.map do |flav|
+            flav.kind
+        end
+        others = Flavor.all.select do |flav|
+            if !cheese_flavors.include?(flav.kind)
+                flav.kind
+            end
+        end
+        @other_flavors = others.map do |other|
+            other.kind
+        end
+
     end
 end
