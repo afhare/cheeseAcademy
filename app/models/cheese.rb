518c458d7fe9aa3ef20894511c9d1c7d7dc64f09
afhare <alexandra.f.hare@gmail.com>
2019-10-29 12:50:05 -0500

    Added appropriate associations to a cheese to be able to visualize the taste-flavor breakdown.

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -1,6 +1,7 @@
 class Cheese < ApplicationRecord
     has_many :flavor_profiles
     has_many :flavors, through: :flavor_profiles
+    has_many :tastes, through: :flavors
     validates :rind, inclusion: { in: %w(bloomy cloth washed waxed fresh natural none)}
     validates :milk, inclusion: { in: %w(cow goat sheep buffalo raw nut-based soy other)}
     validates :texture, inclusion: { in: %w(runny soft semi-soft firm semi-firm hard)}
